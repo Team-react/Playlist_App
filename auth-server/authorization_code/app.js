30cb1066fce34d00ad348127af41da776839be5b
WilliamJGrace <williamjgrace@outlook.com>
2020-05-06 13:14:34 +0100

    added another env

--- auth-server/authorization_code/app.js
+++ auth-server/authorization_code/app.js
@@ -112,7 +112,7 @@ app.get('/callback', function(req, res) {
         });
 
         // we can also pass the token to the browser to make requests from there
-        res.redirect('http://localhost:3000/#' +
+        res.redirect(process.env.REACT_APP_MAIN_PAGE +
           querystring.stringify({
             access_token: access_token,
             refresh_token: refresh_token
