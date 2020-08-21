1544e43dabc5181c37fa1f37092ae6416b69b8ae
liam roberts <liamroberts77@googlemail.com>
2020-08-21 17:02:32 +0100

    fixed bable-jest version problem and adde test file and first test for PlaylistFinaliser

---
+++ client/src/components/PlaylistFinaliser.spec.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import PlaylistFinaliser from './PlaylistFinaliser';
+
+describe('PlaylistFinaliser', () => {
+
+  let wrapper;
+
+beforeEach(() => wrapper = shallow(<PlaylistFinaliser     
+token={''}
+playlistComplete={jest.fn()}
+customPlaylist={{songs:[], playlistDuration:[], list_of_tracks:[]}}
+playlistIsNotComplete={jest.fn()}
+unmountFinaliser={jest.fn()}
+overidePlaylist={jest.fn()}
+getRandomPlaylist={jest.fn()}
+playlistType={jest.fn()} 
+/>));
+
+  it('should render a <div />', () => {
+    console.log(wrapper)
+    expect(wrapper.find('div').length).toEqual(9)
+  })
+})
+
